a8b7e6ae3039dfa73832f6ccd0410a7a3da02bba
Lennon <lemoncode8888@gmail.com>
2026-06-05 05:55:22 +0800

    feat(alert): best-effort Alerter interface with log and recording impls

---
+++ internal/alert/alert_test.go
@@ -0,0 +1,18 @@
+package alert
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/lemonishi/supportsentinel/internal/domain"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRecordingAlerter(t *testing.T) {
+	a := NewRecording()
+	tk := domain.Ticket{ID: uuid.New(), Urgency: domain.UrgencyCritical}
+	require.NoError(t, a.Alert(context.Background(), tk))
+	require.Len(t, a.Sent, 1)
+	require.Equal(t, tk.ID, a.Sent[0].ID)
+}
